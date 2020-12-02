49746474e77ac0ad0c4b936a8b45c9a94accd9dd
NIGERIA\jayeni <jayeni>
2020-12-02 04:11:35 +0100

    Updated model and migrations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+    belongs_to :subject
+
+    validates_presence_of :title
+    validates_numericality_of :price
 end
--- db/migrate/20201202021408_create_subjects.rb
+++ db/migrate/20201202021408_create_subjects.rb
@@ -1,8 +1,14 @@
 class CreateSubjects < ActiveRecord::Migration[6.0]
   def change
     create_table :subjects do |t|
-
+      t.column :name, :string
       t.timestamps
     end
+
+    Subject.create :name => "Physics"
+    Subject.create :name => "Mathematics"
+    Subject.create :name => "Chemistry"
+    Subject.create :name => "Psychology"
+    Subject.create :name => "Geography"
   end
 end
