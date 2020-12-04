fd4301eed63973d812d4133e46d6963f64427671
NIGERIA\jayeni <jayeni>
2020-12-04 16:04:48 +0100

    Created book list view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "book#list"
+  get "books/new", to: "book#new"
+  get "books/:id", to: "book#show" 
 end
